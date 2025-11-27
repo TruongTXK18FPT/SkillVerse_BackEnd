fb12c60c2244a017ab66aad5abc6ca21da309060
TruongTXK18FPT <truongtranxuan41@gmail.com>
2025-11-27 11:41:22 +0700

    huhu

--- src/test/java/com/exe/skillverse_backend/SkillverseBackendApplicationTests.java
+++ src/test/java/com/exe/skillverse_backend/SkillverseBackendApplicationTests.java
@@ -2,9 +2,14 @@ package com.exe.skillverse_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.exe.skillverse_backend.portfolio_service.service.CVGeneratorAIService;
 
 @SpringBootTest
 class SkillverseBackendApplicationTests {
+
+    @MockBean
+    private CVGeneratorAIService cvGeneratorAIService;
 
     @Test
     void contextLoads() {
